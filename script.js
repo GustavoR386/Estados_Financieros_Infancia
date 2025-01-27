c14138420a4ee9a30b055644c6f70316e0729ccc
GustavoR386 <grodriguezfuentes57@correo.unicordoba.edu.co>
2025-01-27 14:05:19 -0500

    script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const toggleMenuButton = document.querySelector('.toggle-menu');
     const sidebar = document.querySelector('.sidebar');
     const content = document.querySelector('.content');
+    const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+    const dropdownContents = document.querySelectorAll('.dropdown-content');
 
     function showTab(tabId) {
         tabContents.forEach(tab => tab.classList.remove('active'));
@@ -14,18 +16,45 @@ document.addEventListener('DOMContentLoaded', function() {
         link.addEventListener('click', function(event) {
             event.preventDefault();
             const tabId = this.getAttribute('data-tab');
-            showTab(tabId);
+            if (tabId) {
+                showTab(tabId);
+            }
+            // Cerrar los desplegables al hacer clic en un enlace
+            dropdownContents.forEach(dropdown => dropdown.style.display = 'none');
+
         });
     });
+
+     // Manejo de los dropdowns
+    dropdownToggles.forEach(toggle => {
+      toggle.addEventListener('click', function(event){
+       event.preventDefault();
+        const dropdown = this.nextElementSibling;
+        if(dropdown.style.display === 'block'){
+          dropdown.style.display = 'none';
+        } else {
+         // Cerrar otros dropdowns
+          dropdownContents.forEach(dropdown => dropdown.style.display = 'none');
+         dropdown.style.display = 'block'
+        }
+
+      });
+    });
+
+      // Cerrar los dropdowns si se hace click fuera de ellos
+      document.addEventListener('click', function (event) {
+          if (!event.target.closest('.dropdown')) {
+           dropdownContents.forEach(dropdown => dropdown.style.display = 'none')
+          }
+     });
+
 
     toggleMenuButton.addEventListener('click', function() {
         sidebar.classList.toggle('collapsed');
         content.classList.toggle('collapsed');
     });
-
-    // Inicialmente mostrar la primera pestaña
-    if (menuLinks.length > 0) {
-       showTab(menuLinks[0].getAttribute('data-tab'));
-    }
     
+    
+    // Inicialmente mostrar la pestaña "hogar"
+    showTab('hogar');
 });
